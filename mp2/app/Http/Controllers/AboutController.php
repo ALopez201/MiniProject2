124c99373fed702b4cda25ebc340aaab7858c5bd
ALopez201 <lopezanthony92@gmail.com>
2019-08-09 13:47:45 -0500

    8.  Create a controller, route, and view to show an about page and add it to the main menu of your site.
    Signed-off-by: ALopez201 <lopezanthony92@gmail.com>

---
+++ mp2/app/Http/Controllers/AboutController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('about');
+    }
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
